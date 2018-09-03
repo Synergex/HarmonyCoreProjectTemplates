e5ae00f63ea84aea28de88174c52b74cf3b1256d
Steve Ives <steve@steveives.com>
2018-09-03 08:57:36 -0700

    Updated templates.

---
+++ UpdateTemplates.bat
@@ -0,0 +1,5 @@
+pushd %~dp0
+copy /y D:\SYNERGEX\HarmonyCore\Templates\*.tpl ProjectTemplates\dotnet-new-harmonycore-synergy\Templates
+copy /y D:\SYNERGEX\HarmonyCore\Templates\*.tpl WorkshopTemplates2018\dotnet-new-hcworkshop-synergy\Templates
+popd
+pause
