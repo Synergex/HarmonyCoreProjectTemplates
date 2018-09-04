26e5004f2590f7914cf1a167a760e841214ea9d5
Steve Ives <stevei@synergex.com>
2018-09-04 16:39:18 -0700

    Updated packages and user defined tokens.

--- GeneratePackage.bat
+++ GeneratePackage.bat
@@ -1,6 +1,13 @@
 pushd %~dp0
+
+if exist Harmony.Core.DemoTemplates.1.0.1.nupkg del /q Harmony.Core.DemoTemplates.1.0.1.nupkg
+nuget pack Harmony.Core.DemoTemplates.nuspec
+
 if exist Harmony.Core.ProjectTemplates.1.0.1.nupkg del /q Harmony.Core.ProjectTemplates.1.0.1.nupkg
 nuget pack Harmony.Core.ProjectTemplates.nuspec
+
+if exist Harmony.Core.WorkshopTemplates2018.1.0.1.nupkg del /q Harmony.Core.WorkshopTemplates2018.1.0.1.nupkg
 nuget pack Harmony.Core.WorkshopTemplates2018.nuspec
+
 popd
 pause
