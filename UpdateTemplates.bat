56c31d79687cb28c83411ecdc8a95c7aaf03df79
Steve Ives <stevei@synergex.com>
2018-09-04 20:48:12 -0700

    Updated templates

--- UpdateTemplates.bat
+++ UpdateTemplates.bat
@@ -1,5 +1,6 @@
 pushd %~dp0
-copy /y D:\SYNERGEX\HarmonyCore\Templates\*.tpl ProjectTemplates\dotnet-new-harmonycore-synergy\Templates
-copy /y D:\SYNERGEX\HarmonyCore\Templates\*.tpl WorkshopTemplates2018\dotnet-new-hcworkshop-synergy\Templates
+copy /y ..\HarmonyCore\Templates\*.tpl DemoTemplates\dotnet-new-hcdemo-synergy\Templates
+copy /y ..\HarmonyCore\Templates\*.tpl ProjectTemplates\dotnet-new-harmonycore-synergy\Templates
+copy /y ..\HarmonyCore\Templates\*.tpl WorkshopTemplates2018\dotnet-new-hcworkshop-synergy\Templates
 popd
 pause
