727b7b13abf684923493f47b94bc608dc3c18de4
Steve Ives <stevei@synergex.com>
2018-09-05 10:43:56 -0700

    Updated templates

--- GeneratePackage.bat
+++ GeneratePackage.bat
@@ -1,8 +1,5 @@
 pushd %~dp0
 
-if exist Harmony.Core.DemoTemplates.1.0.1.nupkg del /q Harmony.Core.DemoTemplates.1.0.1.nupkg
-nuget pack Harmony.Core.DemoTemplates.nuspec
-
 if exist Harmony.Core.ProjectTemplates.1.0.1.nupkg del /q Harmony.Core.ProjectTemplates.1.0.1.nupkg
 nuget pack Harmony.Core.ProjectTemplates.nuspec
 
--- InstallPackage.bat
+++ InstallPackage.bat
@@ -1,5 +1,4 @@
 pushd %~dp0
-dotnet new -i Harmony.Core.DemoTemplates.1.0.1.nupkg
 dotnet new -i Harmony.Core.ProjectTemplates.1.0.1.nupkg
 dotnet new -i Harmony.Core.WorkshopTemplates2018.1.0.1.nupkg
 popd
