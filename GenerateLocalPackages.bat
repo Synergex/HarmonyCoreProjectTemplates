67e7d0de9437da2d03a77bb6e5b09a7f80dd1b8d
Steve Ives <steve@steveives.com>
2018-12-18 19:17:23 -0800

    Updated to Harmony Core 1.0.5

--- GenerateLocalPackages.bat
+++ GenerateLocalPackages.bat
@@ -1,6 +1,6 @@
 pushd %~dp0
 
-if exist Harmony.Core.ProjectTemplates.1.0.3.nupkg del /q Harmony.Core.ProjectTemplates.1.0.3.nupkg
+if exist Harmony.Core.ProjectTemplates.1.0.5.nupkg del /q Harmony.Core.ProjectTemplates.1.0.5.nupkg
 nuget pack Harmony.Core.ProjectTemplates.nuspec
 
 rem if exist Harmony.Core.WorkshopTemplates2018.1.0.1.nupkg del /q Harmony.Core.WorkshopTemplates2018.1.0.1.nupkg
--- InstallLocalPackages.bat
+++ InstallLocalPackages.bat
@@ -1,5 +1,5 @@
 pushd %~dp0
-dotnet new -i Harmony.Core.ProjectTemplates.1.0.3.nupkg
+dotnet new -i Harmony.Core.ProjectTemplates.1.0.5.nupkg
 rem dotnet new -i Harmony.Core.WorkshopTemplates2018.1.0.1.nupkg
 popd
 pause
