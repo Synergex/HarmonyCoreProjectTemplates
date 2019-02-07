168540ad96dad11040f902c1b3b0b401e055796a
Steve Ives <steve@steveives.com>
2019-02-06 22:13:55 -0800

    Updated version numbers to 1.0.35

--- GenerateLocalPackages.bat
+++ GenerateLocalPackages.bat
@@ -1,6 +1,6 @@
 pushd %~dp0
 
-if exist Harmony.Core.ProjectTemplates.1.0.5.nupkg del /q Harmony.Core.ProjectTemplates.1.0.5.nupkg
+if exist Harmony.Core.ProjectTemplates.1.0.35.nupkg del /q Harmony.Core.ProjectTemplates.1.0.35.nupkg
 nuget pack Harmony.Core.ProjectTemplates.nuspec
 
 rem if exist Harmony.Core.WorkshopTemplates2018.1.0.1.nupkg del /q Harmony.Core.WorkshopTemplates2018.1.0.1.nupkg
--- RemoveLocalPackages.bat
+++ RemoveLocalPackages.bat
@@ -1,5 +1,5 @@
 pushd %~dp0
-dotnet new -u Harmony.Core.ProjectTemplates.1.0.5
+dotnet new -u Harmony.Core.ProjectTemplates.1.0.35
 rem dotnet new -u Harmony.Core.WorkshopTemplates2018.1.0.1
 popd
 pause
