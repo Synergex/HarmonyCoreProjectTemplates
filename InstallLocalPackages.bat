78708c71ec7f52842ef216fc5c478377ab4bf0f1
Steve Ives <stevei@synergex.com>
2019-05-29 13:25:08 -0700

    Updated version number

--- InstallLocalPackages.bat
+++ InstallLocalPackages.bat
@@ -1,4 +1,4 @@
 pushd %~dp0
-dotnet new -i Harmony.Core.ProjectTemplates.1.0.53.nupkg
+dotnet new -i Harmony.Core.ProjectTemplates.1.0.54.nupkg
 popd
 pause
--- RemoveLocalPackages.bat
+++ RemoveLocalPackages.bat
@@ -1,4 +1,4 @@
 pushd %~dp0
-dotnet new -u Harmony.Core.ProjectTemplates.1.0.53
+dotnet new -u Harmony.Core.ProjectTemplates.1.0.54
 popd
 pause
