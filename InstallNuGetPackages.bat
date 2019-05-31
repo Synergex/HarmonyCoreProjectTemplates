f3f21b83b3f4196c8e5e4e56cdf4ee95eb443238
Steve Ives <stevei@synergex.com>
2019-05-30 18:32:06 -0700

    Removed DPC2018 templates. They are still on nuget.org if we ever need them.

--- InstallNuGetPackages.bat
+++ InstallNuGetPackages.bat
@@ -1,5 +1,4 @@
 pushd %~dp0
 dotnet new -i Harmony.Core.ProjectTemplates
-rem dotnet new -i Harmony.Core.WorkshopTemplates2018
 popd
 pause
--- UpdateTemplates.bat
+++ UpdateTemplates.bat
@@ -1,4 +1,5 @@
 pushd %~dp0
 copy /y ..\HarmonyCore\Templates\*.tpl ProjectTemplates\dotnet-new-harmonycore-synergy\Templates
+copy /y ..\HarmonyCore\Templates\*.tpl ProjectTemplates\dotnet-new-harmonycorebasic-synergy\Templates
 popd
 pause
