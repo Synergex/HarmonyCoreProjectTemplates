e9f7423e1b3f94d546317fbf66b3a8991bcbe0ce
Steve Ives <steve.ives@synergex.com>
2020-07-12 10:35:00 -0700

    Updated to 3.1.145

--- GenerateLocalPackages.bat
+++ GenerateLocalPackages.bat
@@ -5,7 +5,7 @@ set /p HC_VERSION=<HarmonyCoreVersion.txt
 set NUPKG_FILE=Harmony.Core.ProjectTemplates.%HC_VERSION%.nupkg
 
 if exist %NUPKG_FILE% del /q %NUPKG_FILE%
-nuget pack Harmony.Core.ProjectTemplates.nuspec -properties hc_version=%HC_VERSION%
+nuget pack Harmony.Core.ProjectTemplates.nuspec -NoDefaultExcludes -properties hc_version=%HC_VERSION%
 
 endlocal
 popd
